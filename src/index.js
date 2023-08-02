484678c01f78b125b88986c6685b294f58cca5eb
AleXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-08-02 19:25:58 -0400

    newchanges

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import express from 'express';
+import express from "express";
 
-import productsRoutes from './routes/products.routes.js';
+const app = express()
 
-const app = express();
-import cors from 'cors'
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+})
 
-app.use(cors());
-app.use(express.json());
+app.listen(3000)
+console.log('Server on port:', 3000);
 
-app.use('/api', productsRoutes);
-
-app.listen(3000);
-console.log('Server on port: ', 3000);
