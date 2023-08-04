5da46a6b92a905385766b877b2b6d2c914b5cb8a
AleXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-08-04 16:35:15 -0400

    a

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ app.use(express.urlencoded({extended: false}));
 app.get('/test', async (req,res)=>{
     res.send('test');
 })
+app.get('/ping',  async (req,res)=>{
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0]);
+})
 
 
 app.listen(3000)
