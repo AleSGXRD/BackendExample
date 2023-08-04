585a10f198e5af9c4e1157fbc6f391637f0d5758
AleXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-08-04 16:46:53 -0400

    users

--- src/index.js
+++ src/index.js
@@ -20,10 +20,27 @@ app.get('/ping',  async (req,res)=>{
     const result = await pool.query('SELECT NOW()')
     return res.json(result.rows[0]);
 })
-app.get('/createtable',async (req,res)=>{
+/*app.get('/createtable',async (req,res)=>{
     const result = await pool.query("CREATE TABLE users (id SERIAL PRIMARY KEY,name VARCHAR(40),email TEXT);");
     return res.send('Table created');
+})*/
+app.post('/users', async (req,res)=>{
+    const { name, email} = req.body;
+
+    const response = await pool.query('INSERT INTO users(name,email) VALUES ($1,$2)',[name,email]);
+    console.log(response);
+    res.json({
+        massage: 'User Added Succesfully',
+        body:{
+            user: {name,email}
+        }
+    })
 })
+app.get('/users', async (req,res)=>{
+    const response = await pool.query('SELECT * FROM users');
+    console.log(response.rows);
+    res.status(200).json(response.rows);
+ })
 
 
 app.listen(3000)
